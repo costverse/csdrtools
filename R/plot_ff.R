d9392e72ce1e1d8b47f9a138aab8bc4096670550
Benjamin Berkman <bberkman@TECHNOMICS.NET>
2020-01-19 15:07:16 -0500

    initial function
    -bar chart that allows you to pass a flexfile, x-variable and fill

--- R/plot_ff.R
+++ R/plot_ff.R
@@ -1,1 +1,34 @@
-#Functions to visualize FlexFile data. Includes graphs, charts, etc.
+library(stringr)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(treemap)
+library(tidyr)
+library(magrittr)
+library(ggiraph)
+
+ff_bar_chart <- function(flattened_ff, ind_variable, fill) {
+  
+  column <- enquo(fill)
+  x_axis <- enquo(ind_variable)
+  
+  df <- flattened_ff %>%
+    select(!! x_axis, !! column, value_dollars) %>%
+    group_by(!! x_axis, !! column) %>%
+    summarise(Dollars = sum(value_dollars))
+
+  plot <- ggplot(df, aes_(x_axis, ~Dollars/1000000,
+                  tooltip = column,
+                  data_id = column,
+                  fill = column)) + 
+    scale_y_continuous(labels = scales::dollar) +
+    ylab("Cost ($M)") +
+    theme_minimal() +
+    geom_bar_interactive(stat = "identity")
+  
+  girafe(ggobj = plot)
+  
+}
+
+ff_bar_chart(flattened_ff = flattened_ff, fill = wbs_element_id, ind_variable = start_date)
