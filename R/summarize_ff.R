aec0a32e41c6275d2430b1c3e36b590d2b232107
Benjamin Berkman <bberkman@TECHNOMICS.NET>
2020-01-24 10:35:23 -0500

    -update to tbl_1921()
    adds a condition that allows the user to produce a formatted table or not. Setting formatted_table == TRUE produces the fully formatted kable html table. Setting formatted_table == FALSE just leaves the data in a dataframe, so it could be piped to another function

--- R/summarize_ff.R
+++ R/summarize_ff.R
@@ -6,37 +6,56 @@ library(dplyr)
 library(tidyr)
 
 
-tbl_1921 <- function(flattened_ff, value = c("Dollars", "Hours")){
+tbl_1921 <- function(flattened_ff, value = c("Dollars", "Hours"), formatted_table = c(TRUE, FALSE)){
   
   if(value == "Dollars") {
     
-    flattened_ff %>%
+    temp_1921 <- flattened_ff %>%
       dplyr::group_by(wbs_element_id, wbs_name, atd_or_fac, nonrecurring_or_recurring_id) %>%
       dplyr::summarise(value_dollars = sum(value_dollars), value_hours = sum(value_hours)) %>%
       tidyr::gather(key = value_type, value = value, value_dollars, value_hours) %>%
       tidyr::unite(col = temp, atd_or_fac, nonrecurring_or_recurring_id, sep = "_") %>%
       tidyr::spread(key = temp, value = value) %>%
       dplyr::filter(value_type == "value_dollars") %>%
-      dplyr::select(-value_type) %>%
+      dplyr::select(-value_type)
       
-      knitr::kable(., col.names = c("WBS Element", "WBS Element Name", "Nonrecurring", "Recurring", "Nonrecurring", "Recurring"))  %>%
-      kableExtra::kable_styling(c("striped", "bordered")) %>%
-      kableExtra::add_header_above(c("", "", "Actuals To Date" = 2, "Forecasts At Completion" = 2))
+    if(formatted_table == TRUE) {
+      
+      temp_1921 %>%
+        knitr::kable(., col.names = c("WBS Element", "WBS Element Name", "Nonrecurring", "Recurring", "Nonrecurring", "Recurring"))  %>%
+        kableExtra::kable_styling(c("striped", "bordered")) %>%
+        kableExtra::add_header_above(c("", "", "Actuals To Date" = 2, "Forecasts At Completion" = 2))
+      
+    } else if(formatted_table == FALSE){
+      
+      temp_1921
+      
+    }  
+    
     
   } else if(value == "Hours") {
     
-    flattened_ff %>%
+    temp_1921 <- flattened_ff %>%
       dplyr::group_by(wbs_element_id, wbs_name, atd_or_fac, nonrecurring_or_recurring_id) %>%
       dplyr::summarise(value_dollars = sum(value_dollars), value_hours = sum(value_hours)) %>%
       tidyr::gather(key = value_type, value = value, value_dollars, value_hours) %>%
       tidyr::unite(col = temp, atd_or_fac, nonrecurring_or_recurring_id, sep = "_") %>%
       tidyr::spread(key = temp, value = value) %>%
       dplyr::filter(value_type == "value_hours") %>%
-      dplyr::select(-value_type) %>%
+      dplyr::select(-value_type)
       
-      knitr::kable(., col.names = c("WBS Element", "WBS Element Name", "Nonrecurring", "Recurring", "Nonrecurring", "Recurring"))  %>%
-      kableExtra::kable_styling(c("striped", "bordered")) %>%
-      kableExtra::add_header_above(c("", "", "Actuals To Date" = 2, "Forecasts At Completion" = 2))
+      if(formatted_table == TRUE) {
+        
+        temp_1921 %>%
+          knitr::kable(., col.names = c("WBS Element", "WBS Element Name", "Nonrecurring", "Recurring", "Nonrecurring", "Recurring"))  %>%
+          kableExtra::kable_styling(c("striped", "bordered")) %>%
+          kableExtra::add_header_above(c("", "", "Actuals To Date" = 2, "Forecasts At Completion" = 2))
+        
+      } else if(formatted_table == FALSE){
+        
+        temp_1921
+        
+      }    
     
   }
   
@@ -179,9 +198,11 @@ tbl_WBS_by_date <- function(flattened_ff, timeframe = c("Year", "Month")) {
 
 # test
 
-tbl_1921(flattened_ff ,value = "Dollars")
+# tbl_1921(flattened_ff, value = "Dollars")
 
-tbl_1921_1(flattened_ff, value = "Dollars")
+tbl_1921(flattened_ff, value = "Hours", formatted_table = FALSE)
+
+tbl_1921_1(flattened_ff, value = "Hours")
 
 tbl_top_10_Accounts(flattened_ff)
 
