71fa2f42cdd3e985bfaaaf2f1ae04dd71c32a153
daniel-r-germony <46444796+daniel-r-germony@users.noreply.github.com>
2020-02-05 03:23:05 -0500

    Update scratch_pad.R

--- R/scratch_pad.R
+++ R/scratch_pad.R
@@ -1,8 +1,11 @@
+
+library(tidyverse)
+
 foo <- read_flat_file(here::here("inst", "extdata", "Flat_File.xlsx"))
 
 pivot_flat_file <- function(flat_file, UnitOfMeasure, names_from) {
   
-flat_file$Data %>% 
+  flat_file$Data %>% 
     filter(!is.na(FunctionalCategory)) %>% 
     filter(UnitOfMeasure == {{ UnitOfMeasure }}) %>%
     select(-FullName, -address) %>% 
@@ -24,7 +27,8 @@ foo %>% pivot_flat_file("Dollars", ToDate_AtCompletion) %>%
 foo %>% pivot_flat_file("Dollars", Recurring_Nonrecurring) %>% 
   filter(!is.na(FunctionalElement)) %>% 
   group_by(WBSElementID, WBSElementName, FunctionalCategory, FunctionalElement) %>% 
-  summarise(Recurring = sum(Recurring), Nonrecurring = sum(Nonrecurring))
+  summarise(Recurring = sum(Recurring), Nonrecurring = sum(Nonrecurring)) %>% 
+  mutate(Total = Recurring + Nonrecurring)
 
 foo %>% pivot_flat_file("Dollars", FunctionalElement) %>% 
   group_by(WBSElementID, WBSElementName) %>% 
@@ -50,17 +54,39 @@ foo %>% pivot_flat_file("Dollars", FunctionalElement) %>%
   summarise_if(is.numeric, sum, rm.na = TRUE)
 
 
-    `Direct Engineering Labor` = sum(`Direct Engineering Labor`),
-    `Engineering Overhead` = sum(`Engineering Overhead`),
-    `Direct Tooling Labor` = sum(`Direct Tooling Labor`),
-    `Direct Tooling & Equipment` = sum(`Direct Tooling & Equipment`),
-    `Direct Quality Control Labor` = sum(`Direct Quality Control Labor`),
-    `Direct Manufacturing Labor` = sum(`Direct Manufacturing Labor`),
-    `Manufacturing Operations Overhead` = sum(`Manufacturing Operations Overhead`),
-    `Raw Material` = sum(`Raw Material`),
-    `Purchased Parts` = sum(`Purchased Parts`),
-    `Purchased Equipment` = sum(`Material Handling/Overhead`),
-    `Material Handling/Overhead` = sum(`Material Handling/Overhead`),
-    `Direct-Reporting Subcontractor` = sum(`Direct-Reporting Subcontractor`),
-    `Other Costs Not Shown Elsewhere` = sum(`Other Costs Not Shown Elsewhere`))
 
+`Direct Engineering Labor` = sum(`Direct Engineering Labor`),
+`Engineering Overhead` = sum(`Engineering Overhead`),
+`Direct Tooling Labor` = sum(`Direct Tooling Labor`),
+`Direct Tooling & Equipment` = sum(`Direct Tooling & Equipment`),
+`Direct Quality Control Labor` = sum(`Direct Quality Control Labor`),
+`Direct Manufacturing Labor` = sum(`Direct Manufacturing Labor`),
+`Manufacturing Operations Overhead` = sum(`Manufacturing Operations Overhead`),
+`Raw Material` = sum(`Raw Material`),
+`Purchased Parts` = sum(`Purchased Parts`),
+`Purchased Equipment` = sum(`Material Handling/Overhead`),
+`Material Handling/Overhead` = sum(`Material Handling/Overhead`),
+`Direct-Reporting Subcontractor` = sum(`Direct-Reporting Subcontractor`),
+`Other Costs Not Shown Elsewhere` = sum(`Other Costs Not Shown Elsewhere`))
+
+
+expand.grid(
+  c("Nonrecurring", "Recurring", "Total"),
+  c("ToDate", "AtCompletion"),
+  c("Total",
+    "Direct Engineering Labor", 
+    "Engineering Overhead",
+    "Direct Tooling Labor",
+    "Direct Tooling & Equipment",
+    "Direct Quality Control Labor",
+    "Direct Manufacturing Labor",
+    "Manufacturing Operations Overhead",
+    "Raw Material",
+    "Purchased Parts",
+    "Purchased Equipment",
+    "Material Handling/Overhead",
+    "Direct-Reporting Subcontractor",
+    "Other Costs Not Shown Elsewhere"
+  ))
+
+foo$Definitions %>% filter(!is.na(FunctionalElement)) %>% distinct(FunctionalCategory, FunctionalElement)
